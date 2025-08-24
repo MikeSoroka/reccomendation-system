42568262d8731427a0344ea27de0055c0bf35362
ritter <mihol2006@gmail.com>
2025-08-24 05:12:16 +0300

    Add postgres timeput. Implement redis interactions DB and pooling

--- src/api/redis/repositories/recommender_repository.py
+++ src/api/redis/repositories/recommender_repository.py
@@ -1,5 +1,10 @@
+import ast
+import asyncio
 from typing import Sequence
+from uuid import UUID
 
+import anyio
+import numpy as np
 import pandas as pd
 import torch
 from dependency_injector.wiring import Provide, inject
@@ -8,40 +13,47 @@ from torch.utils.data import Dataset
 
 from src.api.core.container import Container
 from src.api.entities.interaction_table import InteractionTable
+from src.api.redis.redis_utils import RedisUtils, ConversionType
+from src.api.schemas.interactions.read_interaction_model import ReadInteractionModel
 from src.api.services.interactions_service import InteractionsService
 
 
 class MoviesDataset(Dataset):
-    def __init__(
-            self,
-            redis_user_mappings_session: Redis,
-            redis_movie_mappings_session: Redis,
-            interactions: Sequence[InteractionTable] | None = None
-    ):
+    def __init__(self):
         super().__init__()
-        print("Creatin interactions...")
-        self.interactions = interactions
-        print("Creating redis connections...")
-        self.redis_user_mappings = redis_user_mappings_session
-        self.redis_movie_mappings = redis_movie_mappings_session
 
-    @classmethod
     @inject
-    async def create(
-            cls,
-            interactions_service = Provide[Container.interactions_service],
-            redis_user_mappings = Provide[Container.users_mappings_redis_session],
-            redis_movie_mappings = Provide[Container.movies_mappings_redis_session]
+    def __getitem__(
+            self,
+            index: int,
+            interactions_service=Provide[Container.interactions_service],
+            redis_users_mappings=Provide[Container.users_mappings_redis_sync_session],
+            redis_movies_mappings=Provide[Container.movies_mappings_redis_sync_session],
+            redis_interactions_mappings=Provide[Container.interactions_mappings_redis_sync_session]
     ):
+        user_id_str, movie_id_str, interaction_str = RedisUtils.parse_record(redis_interactions_mappings.get(str(index)), ConversionType.MIXED_TUPLE)
+        user_id = eval(user_id_str)
+        movie_id = eval(movie_id_str)
 
-        return cls(redis_user_mappings, redis_movie_mappings, interactions)
+        interaction = np.float32(interaction_str)
+        user_mapping = int(redis_users_mappings.get(str(user_id)))
+        movie_mapping = int(redis_movies_mappings.get(str(movie_id)))
 
-    def __getitem__(self, index):
-        return (
-            self.redis_user_mappings.get(str(self.interactions[index].user_id)),
-            self.redis_movie_mappings.get(str(self.interactions[index].movie_id)),
-            self.interactions[index].rating,
-        )
+        return (user_mapping, movie_mapping), interaction
 
-    def __len__(self):
-        return len(self.interactions)
+    @inject
+    def __len__(
+        self,
+        redis_interactions_mappings=Provide[Container.interactions_mappings_redis_sync_session]
+    ):
+        return redis_interactions_mappings.dbsize()
+
+    @inject
+    def get_dimensions(
+        self,
+        redis_users_mappings=Provide[Container.users_mappings_redis_sync_session],
+        redis_movies_mappings=Provide[Container.movies_mappings_redis_sync_session],
+    ):
+        print("AAAAAAAAAAAAAAAAAAA", redis_users_mappings.dbsize())
+        print("BBBBBBBBBBBBBBBBBBBBB", redis_movies_mappings.dbsize())
+        return redis_users_mappings.dbsize(), redis_movies_mappings.dbsize()
